c53e5a59a808889ba091c778c24432adbb53b80e
Tomohiro Suzuki <suzuki@frog-pod.com>
2018-05-17 16:09:16 +0900

    サンプルアプリ完成

---
+++ Droid/Views/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Views;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using MvvmCross.Platforms.Android.Binding;
+using XamAppCenterSample2018.ViewModels;
+
+namespace XamAppCenterSample2018.Droid
+{
+    [Activity(Label = "XamAppCenterSample2018", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : MvxActivity<MainViewModel>
+    {
+		protected override void OnCreate(Bundle bundle)
+        {
+			base.OnCreate(bundle);
+			RequestWindowFeature(WindowFeatures.NoTitle);
+            SetContentView(Resource.Layout.Main);          
+        }
+
+    }
+}
+
---
+++ XamAppCenterSample2018/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using XamAppCenterSample2018.Services.Interfaces;
+
+namespace XamAppCenterSample2018.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+        string inputText = string.Empty;
+        public string InputText 
+        { 
+            get { return inputText; }
+            set 
+            { 
+                inputText = value;
+                RaisePropertyChanged();
+            }
+        } 
+
+        string translatedText = string.Empty;
+        public string TranslatedText 
+        { 
+            get { return translatedText; }
+            set
+            {
+                translatedText = value;
+                RaisePropertyChanged();
+            }
+        } 
+
+        public IMvxAsyncCommand TranslateCommand { get; private set; }
+
+        public MainViewModel(ITranslateService translateService) : base()
+        {
+            TranslateCommand = new MvxAsyncCommand(async () =>
+            {
+                TranslatedText = await translateService.Translate(InputText);
+            });
+        }
+
+    }
+}
